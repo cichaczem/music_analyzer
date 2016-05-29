08dd4c20bc523b2ba1853b253a75bd70de55e8cb
Dawid Cichuta <cichaczem@gmail.com>
2016-05-29 20:54:10 +0200

    Proper dependencies

--- src/server.js
+++ src/server.js
@@ -63,7 +63,6 @@ const indexHtml = (context) => {
     </head>
     <body>
       <div id="app">{context}</div>
-      <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
       <script src="bundle.js"></script>
